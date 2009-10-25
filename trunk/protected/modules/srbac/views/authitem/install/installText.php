e381edbd7690522f36c336e3e6551dfd03af9c6a
spyros@valor.gr <spyros@valor.gr@fef5c216-b88a-11de-97ce-e783fd196cab>
2009-10-25 13:19:48 +0000

    Added php docs to functions

--- trunk/protected/modules/srbac/views/authitem/install/installText.php
+++ trunk/protected/modules/srbac/views/authitem/install/installText.php
@@ -1,4 +1,4 @@
-<div>
+<div align="left">
   Press install to create the tables needed for srbac module.<br />
   You must have a database, authManager component and the srbac module
   configured in your application's configuration.<br />
@@ -6,19 +6,42 @@
   <?php $this->beginWidget('CTextHighlighter',array('language'=>'php')) ?>
   'modules'=>array('srbac'=>
   array(
-  // Your application's user class (default: User)
-  "userclass"=>"User",
-  // Your users' table user_id column (default: userid)
-  "userid"=>"user_ID",
-  // your users' table username column (default: username)
-  "username"=>"username",
-  // If in debug mode (default: false)
-  "debug"=>true,
-  // The number of items shown in each page (default:15)
-  "pageSize"=>10,
-  // The name of the super user
-  "superUser" =>"Authority",
-  )
+      // Your application's user class (default: User)
+      "userclass"=>"User",
+      // Your users' table user_id column (default: userid)
+      "userid"=>"user_id",
+      // your users' table username column (default: username)
+      "username"=>"user_name",
+      // If in debug mode (default: false)
+      // In debug mode every user (even guest) can admin srbac, also
+      //if you use internationalization untranslated words/phrases
+      //will be marked with a red star
+      "debug"=>true,
+      // The number of items shown in each page (default:15)
+      "pageSize"=>8,
+      // The name of the super user
+      "superUser" =>"Authority",
+      //The name of the css to use
+      "css"=>"",
+      //The layout to use
+      "layout"=>"application.views.layouts.admin",
+      //The not authorized page
+      "notAuthorizedView"=>"application.views.site.unauthorized",
+      // The always allowed actions
+      "alwaysAllowed"=>array(
+        'SiteLogin','SiteLogout','SiteIndex','SiteAdmin','SiteError',
+        'SiteContact'),
+      // The operationa assigned to users
+      "userActions"=>array(
+        "Show","View","List"
+      ),
+      // The number of lines of the listboxes
+      "listBoxNumberOfLines" => 10,
+      // The path to the custom images relative to basePath (default the srbac images path)
+      //"imagesPath"=>"../images",
+      //The icons pack to use (noia, tango)
+      "imagesPack"=>"noia",
+    )
   ),
   <?php $this->endWidget('CTextHighlighter') ?>
   The names of the tables are set in your authManager configuration.<br />
