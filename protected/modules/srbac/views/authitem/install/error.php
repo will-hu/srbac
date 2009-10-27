055318f9f9e7a849f298e7651caad5f96aceb3b0
spyros <spyros@valor.gr>
2009-10-27 18:02:49 +0000

    Set error message as a variable instead of static text

--- protected/modules/srbac/views/authitem/install/error.php
+++ protected/modules/srbac/views/authitem/install/error.php
@@ -1,4 +1,4 @@
 <h3><?php echo Helper::translate('srbac','Install Srbac')?></h3>
- <div>
-    <?php echo Helper::translate('srbac','Error while installing srbac.<br />Please check your database and try again'); ?>
- </div>
+<div class="message">
+  <?php echo $error; ?>
+</div>
