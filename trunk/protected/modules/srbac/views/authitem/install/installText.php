aebf60de0e8d47b666ad6a9cc0c0684e9f191aa9
spyros@valor.gr <spyros@valor.gr@fef5c216-b88a-11de-97ce-e783fd196cab>
2009-10-28 09:57:47 +0000

    Added iconText attribute in example srbac configuration

--- trunk/protected/modules/srbac/views/authitem/install/installText.php
+++ trunk/protected/modules/srbac/views/authitem/install/installText.php
@@ -57,6 +57,8 @@
       //"imagesPath"=>"../images",
       //The icons pack to use (noia, tango)
       "imagesPack"=>"noia",
+      // Whether to show text next to the menu icons (default false)
+      "iconText"=>true,
     )
   ),
   <?php $this->endWidget('CTextHighlighter') ?>
