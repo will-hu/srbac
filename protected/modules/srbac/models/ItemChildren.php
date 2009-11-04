fca49d9b96adfcab31cc1e4a843da25f4ba1a1ed
spyros <spyros@valor.gr>
2009-11-04 11:59:40 +0000

    update files for Yii 1.1 compatibility (fixes issue 5)

--- protected/modules/srbac/models/ItemChildren.php
+++ protected/modules/srbac/models/ItemChildren.php
@@ -14,47 +14,43 @@
  * @package srbac.models
  * @since 1.0.0
  */
- 
-class ItemChildren extends CActiveRecord
-{
-	/**
-     * The followings are the available columns in table 'itemchildren':
-	 * @var string $parent
-	 * @var string $child
-	 */
 
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @return CActiveRecord the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+class ItemChildren extends CActiveRecord {
+/**
+ * The followings are the available columns in table 'itemchildren':
+ * @var string $parent
+ * @var string $child
+ */
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return Yii::app()->authManager->itemChildTable;
-	}
+/**
+ * Returns the static model of the specified AR class.
+ * @return CActiveRecord the static model class
+ */
+    public static function model($className=__CLASS__) {
+        return parent::model($className);
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		return array(
-		);
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName() {
+        return Yii::app()->authManager->itemChildTable;
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		return array(
-			);
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules() {
+        return array(
+            array('parent,child','safe')
+        );
+    }
+
+    /**
+     * @return array relational rules.
+     */
+    public function relations() {
+        return array(
+        );
+    }
 }
