92977ba34ba57b6f97fa0821939949e5be9c0790
spyros@valor.gr <spyros@valor.gr@fef5c216-b88a-11de-97ce-e783fd196cab>
2009-11-09 07:02:03 +0000

    Added help icon that toggles the help text on install page

--- trunk/protected/modules/srbac/views/authitem/install/install.php
+++ trunk/protected/modules/srbac/views/authitem/install/install.php
@@ -14,18 +14,40 @@
  * @since 1.0.0
  */
 ?>
+<?php
+$script = "
+jQuery('#help_handle').click(function(){
+$('#help').toggle('1000');
+});";
+
+Yii::app()->clientScript->registerScript("cb",$script,CClientScript::POS_READY);
+?>
 <?php $error = false; $disabled = array(); ?>
 <h3><?php echo Helper::translate('srbac','Install Srbac')?></h3>
 <div class="srbac">
+  <div id="help_handle" class="iconBox" style="float:right">
+    <?php echo
+    CHtml::image($this->module->getIconsPath().'/help.png',
+        Helper::translate('srbac', 'Help'),
+        array('class'=>'icon',
+        'title'=>Helper::translate('srbac','Help'),
+        'border'=>0
+        ))." " .
+        ($this->module->iconText ?
+        Helper::translate('srbac','Help') :
+        "");
+    ?>
+  </div>
+  <br />
   <?php echo CHtml::beginForm(); ?>
-  <div>
+  <div id="help" style="display:none">
     <?php $this->renderPartial(Yii::app()->findLocalizedFile('install/installText'))?>
   </div>
   <div>
     <?php echo Helper::translate('srbac','Your Database, AuthManager and srbac settings:'); ?>
     <table class="srbacDataGrid" width="'100%">
       <?php if(Yii::app()->authManager instanceof CDbAuthManager) { ?>
-      
+
         <?php try { ?>
       <tr>
         <th colspan="2"><?php echo Helper::translate('srbac','Database');?></th>
@@ -77,9 +99,9 @@
         <th colspan="2"><?php echo Helper::translate('srbac','srbac');?></th>
       </tr>
         <?php foreach ($this->module->getAttributes() as $key=>$value) { ?>
-        <?php $check = Helper::checkInstall($key,$value); ?>
-        <?php echo $check[0]; ?>
-      <?php if($check[1] == 1)$error = true;?>
+          <?php $check = Helper::checkInstall($key,$value); ?>
+          <?php echo $check[0]; ?>
+          <?php if($check[1] == 1)$error = true;?>
         <?php } ?>
       <?php  } catch(CException $e ) { ?>
       <tr>
@@ -101,11 +123,11 @@
         <td><?php echo Yii::getVersion()?></td>
         <?php } else {?>
         <td style="color:red;font-weight:bold"><?php echo Yii::getVersion().
-              "  <br /> ".
-              Helper::translate("srbac","Wrong Yii version, lower required version is")." ".Helper::findModule("srbac")->getSupportedYiiVersion(); ?></td>
-          <?php
-          $error =true;
-        } ?>
+                "  <br /> ".
+                Helper::translate("srbac","Wrong Yii version, lower required version is")." ".Helper::findModule("srbac")->getSupportedYiiVersion(); ?></td>
+            <?php
+            $error =true;
+          } ?>
       </tr>
     </table>
   </div>
