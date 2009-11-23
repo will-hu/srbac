26af5d1ce0196c9d7aa710112413eec9367d181a
spyros@valor.gr <spyros@valor.gr@fef5c216-b88a-11de-97ce-e783fd196cab>
2009-11-23 18:18:34 +0000

    Add Yii version in default footer

--- trunk/protected/modules/srbac/views/authitem/footer.php
+++ trunk/protected/modules/srbac/views/authitem/footer.php
@@ -15,6 +15,7 @@
  */
  ?>
 <p align="right">
+Yii Version <?php echo Yii::getVersion(); ?>,&nbsp;
 Srbac Version <?php echo $this->module->getVersion() ?> - Copyright &copy; 2009 by
 <?php echo CHtml::link("Spyros",
   "http://www.yiiframework.com/forum/index.php?/user/1089-spyros/",
